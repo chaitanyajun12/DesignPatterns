f31086b91b0febebfac3303a997c9772a46f39fe
Krishna Chaitanya M <chaitanya.jun12@gmail.com>
2015-12-10 23:56:11 +0530

    doc update

--- src/com/design/patterns/creational/prototype/Client.java
+++ src/com/design/patterns/creational/prototype/Client.java
@@ -5,6 +5,9 @@ package com.design.patterns.creational.prototype;
  * 
  * This creational design pattern caches the prototype (instance) of the class
  * and returns the clone of the object.
+ * 
+ * The difference when compared to the factory pattern is it returns the object itself
+ * directly, whereas prototype design pattern returns the clone of the instance/prototype.
  */
 public class Client {
 	
